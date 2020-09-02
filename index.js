8f22d214001128a1e8b08ecf9856aed94cf3211a
Pedro Ernesto Tramontina <pedro.tramontina@gmail.com>
2020-09-02 20:26:12 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+require('./db/mongoose')
+
+const userRouter = require('./routers/user')
+
+const app = express()
+const port = process.env.PORT
+
+app.use(express.json())
+app.use(userRouter)
+app.use(groupRouter)
+app.use(deviceRouter)
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`)
+})
